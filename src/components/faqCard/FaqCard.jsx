1ebb065648cf6b37b430e8f0b08e9b6288db1450
micheaol <micheaol80@gmail.com>
2024-09-03 11:54:16 +0100

    Comment out react-icons/io5 from the FaqCard component

--- src/components/faqCard/FaqCard.jsx
+++ src/components/faqCard/FaqCard.jsx
@@ -2,16 +2,14 @@ import React, { useState } from "react";
 import RightIcon from "../../assets/right.png";
 import ChevronIcon from "../../assets/chevron-down.png";
 import ChevronUpIcon from "../../assets/chevron-up.webp"; // Icon for open state
-import { IoChevronForwardSharp } from "react-icons/io5";
-
-
+// import { IoChevronForwardSharp } from "react-icons/io5";
 
 const accordionData = [
   {
     id: 1,
     title: "What services does Homerun offer?",
     desc: "What services does Homerun offer? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat. ui officia deserunt mollit anim id estlaborum. Sed ut perspiciatis unde omnis iste natus error sitvoluptatem accusantium doloremque laudantium",
-    chevron: <IoChevronForwardSharp />
+    // chevron: <IoChevronForwardSharp />
   },
   {
     id: 2,
@@ -49,9 +47,7 @@ const FaqCard = () => {
               >
                 <h4>{accordion.title}</h4>
                 <img
-                  src={
-                    accordionId === accordion.id ? ChevronUpIcon : RightIcon
-                  }
+                  src={accordionId === accordion.id ? ChevronUpIcon : RightIcon}
                   className="accordion-icon"
                   alt="icon"
                 />
