5f8c58214bffe14e41966b3943a55fa03edbd6ec
micheaol <micheaol80@gmail.com>
2024-09-29 21:08:10 +0100

    Add logic to display process

--- src/components/requirement/Requirement.jsx
+++ src/components/requirement/Requirement.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Requirement.css";
+import { FaChevronRight, FaChevronDown } from "react-icons/fa";
 
 const Requirement = ({ serviceData }) => {
   const [showRequirement, setShowRequirement] = useState(false);
@@ -13,7 +14,11 @@ const Requirement = ({ serviceData }) => {
       <div className="requirement-main-header-wrapper">
         <div className="requirement-header" onClick={handleOpenRequirement}>
           <h2>Requirements</h2>
-          <img src="/images/chevron-forw.png" alt="" />
+          {showRequirement ? (
+            <FaChevronDown size={25} />
+          ) : (
+            <FaChevronRight size={25} />
+          )}
         </div>
         <p>Please see a list of what is required from you</p>
       </div>
