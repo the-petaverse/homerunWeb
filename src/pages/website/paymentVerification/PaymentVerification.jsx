8097951045882295ae392c3e7481b21972e5b223
micheaol <micheaol80@gmail.com>
2024-10-20 16:47:07 +0100

    Add logic to show total amout to be paid

--- src/pages/website/paymentVerification/PaymentVerification.jsx
+++ src/pages/website/paymentVerification/PaymentVerification.jsx
@@ -50,7 +50,7 @@ const PaymentVerification = () => {
           Once again, thank you for trusting us with your errand
         </p>
       </div>
-      <div className="lg:w-[30%] sm:w-[40%] max-sm:w-[100%] mr-auto ml-auto mt-12">
+      <div className="w-[100%] mr-auto ml-auto mt-12">
         <CustomButton
           title="Back to Dashboard"
           btnOnClick={handleBackToDashboard}
