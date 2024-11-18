127845189b2557fc9514fd50d01104b6e59792e4
micheaol <micheaol80@gmail.com>
2024-11-18 14:27:01 +0100

    Make service card a component

--- src/components/MainServiceCard.jsx
+++ src/components/MainServiceCard.jsx
@@ -5,12 +5,12 @@ import ComigSoonOverlay from "./ComigSoonOverlay";
 const MainServiceCard = ({ service, handleNavigate }) => {
   return (
     <div
-      className="w-[58dvw] flex flex-col laptop:flex-row mt-10"
+      className="w-[56dvw] flex flex-col laptop:flex-row mt-10 border rounded-3xl"
       key={service.id}
     >
-      <div className="relative h-[100%] w-[100%] laptop:w-[50%] border">
+      <div className="relative h-[100%] w-[100%] laptop:w-[40%] ">
         <Image
-          className="w-full"
+          className="w-full rounded-t-3xl laptop:rounded-l-3xl"
           cloudName="petaverse"
           publicId={service.category_image_url}
         >
@@ -18,9 +18,9 @@ const MainServiceCard = ({ service, handleNavigate }) => {
         </Image>
         <ComigSoonOverlay />
       </div>
-      <div className="w-[100%] laptop:w-[100%] border p-5 flex flex-col justify-between">
+      <div className="w-[100%] laptop:w-[100%]  p-5 flex flex-col justify-between  laptop:rounded-r-3xl rounded-b-3xl">
         {/* <div className="card-right-holder"> */}
-        <h1 className="font-sans text-3xl font-bold">
+        <h1 className="font-sans text-3xl tablet:text-4xl font-bold">
           {service.category_name}
         </h1>
         <p className="text-xl">{service.category_details}</p>
