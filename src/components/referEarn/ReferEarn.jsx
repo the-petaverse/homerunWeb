7983740e19c390ed27d4aab8b4fc03909bf24756
micheaol <micheaol80@gmail.com>
2024-12-04 08:00:52 +0100

    Refactor the refer and win data

--- src/components/referEarn/ReferEarn.jsx
+++ src/components/referEarn/ReferEarn.jsx
@@ -5,30 +5,8 @@ import { FiCheckSquare } from "react-icons/fi";
 
 import { useGetUserReferrerCodeQuery } from "../../services/referrerSystem/referrerSystem";
 import { useAuthContext } from "../../customHooks/useAuthContext";
+import { inviteData } from "../../data/referAndWin";
 
-const inviteData = [
-  {
-    id: "1",
-    title: "Invite Your Friends",
-    icon: "/images/earn-1.png",
-    details:
-      "Copy your referral code or share your referral link with friends and family.",
-  },
-  {
-    id: "2",
-    title: " Regularly Shop and Run Errands on Homerun",
-    icon: "/images/earn-2.png",
-    details:
-      "Use Homerun app regularly for shopping and bookings and document retrieval  to earn points.",
-  },
-  {
-    id: "3",
-    title: "You Make Savings",
-    icon: "/images/earn-3.png",
-    details:
-      "You and your friend earn points. Also for every transaction, you also earn money.",
-  },
-];
 const ReferEarn = () => {
   const [refererCode, setRefereCode] = useState("");
   const { data, isLoading, error, isSuccess } = useGetUserReferrerCodeQuery();
---
+++ src/data/referAndWin.js
@@ -0,0 +1,23 @@
+export const inviteData = [
+  {
+    id: "1",
+    title: "Invite Your Friends",
+    icon: "/images/earn-1.png",
+    details:
+      "Copy your referral code or share your referral link with friends and family.",
+  },
+  {
+    id: "2",
+    title: " Regularly Shop and Run Errands on Homerun",
+    icon: "/images/earn-2.png",
+    details:
+      "Use Homerun app regularly for shopping and bookings and document retrieval  to earn points.",
+  },
+  {
+    id: "3",
+    title: "You Make Savings",
+    icon: "/images/earn-3.png",
+    details:
+      "You and your friend earn points. Also for every transaction, you also earn money.",
+  },
+];
